20b8e03e7cd4fc6a5c9cbc730c25772a199887cd
daviromao <davi.romao0210@gmail.com>
2023-04-12 21:09:45 -0300

    refactor: make some changes

--- libs/arithmetic.c
+++ libs/arithmetic.c
@@ -1,4 +1,5 @@
 #include <gmp.h>
+#include "utils.c"
 
 typedef mpz_t bigint;
 
@@ -12,7 +13,7 @@ void sub(bigint result, bigint num1, bigint num2)
     mpz_sub(result, num1, num2);
 }
 
-void mul(bigint result, bigint num1, bigint num2)
+void mult(bigint result, bigint num1, bigint num2)
 {
     mpz_mul(result, num1, num2);
 }
@@ -22,24 +23,64 @@ void div(bigint result, bigint num1, bigint num2)
     mpz_div(result, num1, num2);
 }
 
+void div_int(bigint result, bigint num1, int num2)
+{
+    mpz_div_ui(result, num1, num2);
+}
+
 void mod(bigint result, bigint num1, bigint num2)
 {
     mpz_mod(result, num1, num2);
 }
 
+int mod_int(bigint num1, int num2)
+{
+    return mpz_fdiv_ui(num1, num2);
+}
+
 void mdc(bigint result, bigint num1, bigint num2)
 {
-    if(mpz_cmp_si(num2, 0) == 0)
+    if(compare_int(num2, 0) == 0)
     {
         mpz_set(result, num1);
         return;
     }
 
-    bigint temp;
-    mpz_init(temp);
-    mpz_mod(temp, num1, num2);
-    mdc(result, num2, temp);
-    mpz_clear(temp);
+    bigint resto;
+    mpz_init(resto);
+    mod(resto, num1, num2);
+    mdc(result, num2, resto);
+    mpz_clear(resto);
 }
 
+void mod_exp(bigint result, bigint base, bigint exp, bigint m){
 
+    if(compare_int(m, 1) == 0){
+        set_int(result, 0);
+        return;
+    }
+
+    bigint temp_mult;
+    mpz_init(temp_mult);
+
+    bigint base_t;
+    mpz_init(base_t);
+
+    mod(base_t, base, m);
+    set_int(result, 1);
+
+    while (compare_int(exp, 0) > 0){
+        if(mod_int(exp, 2) == 1){
+            mult(temp_mult, result, base_t);
+            mod(result, temp_mult, m);
+        }
+
+        mult(temp_mult, base_t, base_t);
+        mod(base_t, temp_mult, m);
+
+        div_int(exp, exp, 2);
+    }
+
+    mpz_clear(temp_mult);
+    mpz_clear(base_t);
+}
