ba1601246ec0774fad8b2df4c6cf8642cee87e92
Gabriel Gomes <gomesal2009@gmail.com>
2023-04-13 17:24:15 -0300

    adicionar funcao do inverso

--- libs/inverse.c
+++ libs/inverse.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 #include <gmp.h>
 
-void inverse(mpz_t result, mpz_t a, mpz_t m, int *cont, mpz_t s, mpz_t t) {
+void _inverse(mpz_t mdc, mpz_t a, mpz_t m, int *cont, mpz_t s, mpz_t t)
+{
     mpz_t mod, div, zero, um, neg_div, mult, sum;
     mpz_inits(mod, div, zero, neg_div, mult, sum, NULL);
     mpz_init_set_str(um, "1", 10);
 
-
     mpz_fdiv_qr(div, mod, a, m);
     mpz_neg(neg_div, div);
-    
-    gmp_printf("negdiv: %Zd ", neg_div);
-    gmp_printf("A: %Zd M: %Zd div: %Zd mod: %Zd iteracoes: %d\n", a, m, div, mod, *cont);
 
-    if(mpz_cmp(mod, zero) == 0) {
-        mpz_set(result, m);
+    if (mpz_cmp(mod, zero) == 0)
+    {
+        mpz_set(mdc, m);
         return;
     }
 
-    inverse(result, m, mod, cont, s, t);
+    _inverse(mdc, m, mod, cont, s, t);
 
-    gmp_printf("s: %Zd t: %Zd\n", s, t);    
     if (*cont == 1)
     {
         mpz_set(s, um);
@@ -39,23 +36,38 @@ void inverse(mpz_t result, mpz_t a, mpz_t m, int *cont, mpz_t s, mpz_t t) {
         mpz_set(t, sum);
     }
 
-    (*cont)++;    
-       
+    (*cont)++;
+
     mpz_clears(mod, div, zero, um, neg_div, mult, sum, NULL);
 }
 
-int main() {
-    mpz_t result, a, m, s, t;
+void getInverse(mpz_t inv, mpz_t a, mpz_t m)
+{
+    mpz_t mdc, s, t, mult1, mult2, soma, aux, zero;
     int cont = 1, *c = &cont;
-    mpz_inits(result, s, t, NULL);
+    mpz_inits(mdc, s, t, mult1, mult2, soma, aux, zero, NULL);
 
-    mpz_init_set_str(a, "144", 10);
-    mpz_init_set_str(m, "233", 10);
     mpz_init_set_str(s, "1", 10);
 
-    inverse(result, a, m, c, s, t);
+    _inverse(mdc, a, m, c, s, t);
 
-    gmp_printf("\nresto: %Zd\ns: %Zd\ncont: %d\n", result, s, cont);
+    mpz_mul(mult1, s, a);
+    mpz_mul(mult2, t, m);
+    mpz_add(soma, mult1, mult2);
 
-    return 0;
+    if (!(mpz_cmp(soma, mdc) == 0))
+    {
+        mpz_set(aux, s);
+        mpz_set(s, t);
+        mpz_set(t, aux);
+    }
+
+    if (mpz_cmp(s, zero) < 0)
+    {
+        mpz_add(s, s, m);
+    }
+
+    mpz_set(inv, s);
+
+    mpz_clears(mdc, s, t, mult1, mult2, soma, aux, zero, NULL);
 }
