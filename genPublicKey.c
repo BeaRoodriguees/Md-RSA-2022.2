da0752bcafa7200c528925023af904958ee6d58d
daviromao <davi.romao0210@gmail.com>
2023-04-17 10:15:59 -0300

    feat: remove unnecessary variable

--- genPublicKey.c
+++ genPublicKey.c
@@ -20,7 +20,7 @@ int exportPublicKey(const mpz_t n, const mpz_t e)
     }
 }
 
-void generate_public_key(const mpz_t p, const mpz_t q, const mpz_t e, mpz_t n, mpz_t d) {
+void generate_public_key(const mpz_t p, const mpz_t q, const mpz_t e, mpz_t n) {
     mpz_t p_minus_1, q_minus_1, phi_n, gcd;
     mpz_inits(p_minus_1, q_minus_1, phi_n, gcd, NULL);
 
@@ -32,9 +32,6 @@ void generate_public_key(const mpz_t p, const mpz_t q, const mpz_t e, mpz_t n, m
     mpz_sub_ui(q_minus_1, q, 1);
     mpz_mul(phi_n, p_minus_1, q_minus_1);
 
-    // d = e^-1 mod phi_n
-    mpz_invert(d, e, phi_n);
-
     // liberar Memória
     mpz_clears(p_minus_1, q_minus_1, phi_n, gcd, NULL);
     exportPublicKey(n, e);
@@ -42,8 +39,8 @@ void generate_public_key(const mpz_t p, const mpz_t q, const mpz_t e, mpz_t n, m
 
 int loadPqeKey()
 {
-    mpz_t p, q, e, n, d;
-    mpz_inits(p, q, e, n, d, NULL);
+    mpz_t p, q, e, n;
+    mpz_inits(p, q, e, n, NULL);
 
     FILE *pqeKey = fopen("pqekey.txt", "r");
     if (pqeKey)
@@ -52,7 +49,7 @@ int loadPqeKey()
         mpz_inp_str(q, pqeKey, 10);
         mpz_inp_str(e, pqeKey, 10);
         fclose(pqeKey);
-        generate_public_key(p, q, e, n, d);
+        generate_public_key(p, q, e, n);
         return 1;
     }
     else
