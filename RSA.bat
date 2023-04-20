ed706096e5456f84756704f9b62530a072333497
Felipe Lira <128178415+Eu-FelipeDev@users.noreply.github.com>
2023-04-20 18:19:17 -0300

    Execute .py as Adm

---
+++ RSA.bat
@@ -0,0 +1,20 @@
+color f0
+mode con cols=98 lines=32
+echo off
+cls
+Title Rsa Admin Mode ^| FsystemBR
+::===========================================================================
+fsutil dirty query %systemdrive%  >nul 2>&1 || (
+echo ==== ERRO ====
+echo Este Script requer Privilegios administrativos.
+echo Abra o script com clique direito e escolha 'Executar como administrador'
+echo.
+echo Pressione alguma tecla para sair...
+pause >nul
+exit
+)
+::===========================================================================
+setlocal EnableExtensions EnableDelayedExpansion
+pushd "%~dp0"
+cd /d "%~dp0"
+start python3 RSA.py
