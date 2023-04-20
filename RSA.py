de0a073f64aa46f0965d6e51fbaa16a422f91143
Eu-FelipeDev <fls@ic.ufal.br>
2023-04-20 18:15:00 -0300

    Finished interface

--- RSA.py
+++ RSA.py
@@ -2,8 +2,9 @@ import customtkinter
 import os
 import subprocess
 from tkinter import filedialog
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 import platform
+import emoji
 
 
 
@@ -28,8 +29,8 @@ class App(customtkinter.CTk):
         # load images with light and dark mode image
         image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "img")
         self.logo_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "ufal.png")), size=(36, 56))
-        #self.large_test_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "large_test_image.png")), size=(500, 150))
-        #self.image_icon_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "image_icon_light.png")), size=(20, 20))
+        self.smile = customtkinter.CTkImage(Image.open(os.path.join(image_path, "smile.png")), size=(15, 15))
+        self.heart = customtkinter.CTkImage(Image.open(os.path.join(image_path, "heart.png")), size=(15, 15))
         self.home_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "home_dark.png")),
                                                 dark_image=Image.open(os.path.join(image_path, "home_light.png")), size=(30, 30))
         self.key_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "key_dark.png")),
@@ -68,10 +69,12 @@ class App(customtkinter.CTk):
                                                       image=self.decrypt_image, anchor="w", command=self.frame_4_button_event)
         self.frame_4_button.grid(row=4, column=0, sticky="ew")
 
+        self.appearance_mode_label = customtkinter.CTkLabel(self.navigation_frame, text="Tema:", font=customtkinter.CTkFont(size=15))
+        self.appearance_mode_label.grid(row=5, column=0, padx=(0,0), pady=(20,0), sticky="s")
 
-        self.appearance_mode_menu = customtkinter.CTkOptionMenu(self.navigation_frame, values=["Dark", "Light","System"],
+        self.appearance_mode_menu = customtkinter.CTkOptionMenu(self.navigation_frame, values=["System", "Dark","Light"],
                                                                 command=self.change_appearance_mode_event)
-        self.appearance_mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
+        self.appearance_mode_menu.grid(row=6, column=0, padx=0, pady=(0,20), sticky="s")
 
         # create home frame
         self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
@@ -79,13 +82,18 @@ class App(customtkinter.CTk):
         self.home_frame_label = customtkinter.CTkLabel(self.home_frame, text="Bem-Vindo!\n Para Iniciar, Selecione uma opção ao lado.",
                                                              compound="center", font=customtkinter.CTkFont(size=18, weight="bold"))
         self.home_frame_label.grid(row=0, column=0, padx=0, pady=60)
-        self.home_frame_label2 = customtkinter.CTkLabel(self.home_frame, text="Projeto Desenvolvido por:\n Arthur Pontes de Miranda Ramos Soares\nBeatriz Rodrigues Cavalcante\nDavi Da Silva Romao\nFelipe Lira Da Silva\nGabriel Gomes De Oliveira\n\n Professor: Bruno Pimentel\n Disciplina:Matematica Discreta",
+        self.home_frame_label2 = customtkinter.CTkLabel(self.home_frame, text="Projeto Desenvolvido por:\n Arthur Pontes de Miranda Ramos Soares\nBeatriz Rodrigues Cavalcante\nDavi Da Silva Romao\nFelipe Lira Da Silva\nGabriel Gomes De Oliveira\n\n Professor: Bruno Pimentel\n Disciplina: Matemática Discreta",
                                                              compound="center", font=customtkinter.CTkFont(size=18, weight="bold"))
         self.home_frame_label2.grid(row=1, column=0, padx=0, pady=40)
-        self.home_frame_label1 = customtkinter.CTkLabel(self.home_frame, text="Algoritmo RSA desenvolvido em C.\n Front-End desenvolvido em Python.",
+        self.home_frame_label1 = customtkinter.CTkLabel(self.home_frame, text="Algoritmo RSA desenvolvido em C.\nInteface desenvolvida em Python.",
                                                              compound="center", font=customtkinter.CTkFont(size=14, weight="bold"))
-        self.home_frame_label1.grid(row=2, column=0, padx=0, pady=100)
-
+        self.home_frame_label1.grid(row=2, column=0, padx=(0,20), pady=100)
+        self.home_frame_label1 = customtkinter.CTkLabel(self.home_frame, text="", image=self.smile,
+                                                             compound="center", font=customtkinter.CTkFont(size=14, weight="bold"))
+        self.home_frame_label1.grid(row=2, column=0, padx=(280,25), pady=(69,90))
+        self.home_frame_label1 = customtkinter.CTkLabel(self.home_frame, text="", image=self.heart,
+                                                             compound="center", font=customtkinter.CTkFont(size=14, weight="bold"))
+        self.home_frame_label1.grid(row=2, column=0, padx=(280,31), pady=(100,82))
 
         # create second frame
         self.second_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
@@ -252,6 +260,11 @@ class App(customtkinter.CTk):
             line = process.stdout.decode('utf-8').rstrip()
         errcode = process.returncode
         self.status1.configure(text=line)
+        with open('mensagem_encriptada.txt','r') as msg:
+            msgEncrypt = msg.read()
+            self.crypto_text.delete("0.0", "end")
+            self.crypto_text.insert("0.0", text=msgEncrypt)
+            msg.close()
 
     def decryptMsg(self):
         file = open('privatekey.txt', 'w')
@@ -272,6 +285,11 @@ class App(customtkinter.CTk):
             line = process.stdout.decode('utf-8').rstrip()
         errcode = process.returncode
         self.status2.configure(text=line)
+        with open('mensagem_desencriptada.txt','r') as msg:
+            msgDecrypt = msg.read()
+            self.des_text.delete("0.0", "end")
+            self.des_text.insert("0.0", text=msgDecrypt)
+            msg.close()
 
 
     def change_appearance_mode_event(self, new_appearance_mode):
@@ -287,6 +305,9 @@ def compile_archives(os_name):
     subprocess.run(["gcc", "decrypt.c", "-o", "./bin/decrypt"+extension_name, "-lgmp"])
     subprocess.run(["gcc", "encrypt.c", "-o", "./bin/encrypt"+extension_name, "-lgmp"])
     subprocess.run(["gcc", "genPublicKey.c", "-o", "./bin/genPublicKey"+extension_name, "-lgmp"])
+
+        
+    
 
 if __name__ == "__main__":
     os_name = platform.system()
